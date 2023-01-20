eb57ba795885f00e4f8fc6f64ce64bd9a0d2633a
saif <saifalishaikh2021@gmail.com>
2023-01-20 16:27:03 +0530

    first commit

---
+++ jQuery_WebAPI_StoredProc_CRUD_MVC/Controllers/AjaxAPIController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Http;
+using System.Linq;
+
+namespace jQuery_WebAPI_StoredProc_CRUD_MVC.Controllers
+{
+    public class AjaxAPIController : ApiController
+    {
+        [Route("api/AjaxAPI/InsertCustomer")]
+        [HttpPost]
+        public Customer InsertCustomer(Customer _customer)
+        {
+            using (CustomersEntities entities = new CustomersEntities())
+            {
+                _customer = entities.Customers_PerformCRUD("INSERT", null, _customer.Name, _customer.Country).FirstOrDefault();
+            }
+
+            return _customer;
+        }
+
+        [Route("api/AjaxAPI/UpdateCustomer")]
+        [HttpPost]
+        public bool UpdateCustomer(Customer _customer)
+        {
+            using (CustomersEntities entities = new CustomersEntities())
+            {
+                entities.Customers_PerformCRUD("UPDATE", _customer.CustomerId, _customer.Name, _customer.Country);
+            }
+
+            return true;
+        }
+
+        [Route("api/AjaxAPI/DeleteCustomer")]
+        [HttpPost]
+        public void DeleteCustomer(Customer _customer)
+        {
+            using (CustomersEntities entities = new CustomersEntities())
+            {
+                entities.Customers_PerformCRUD("DELETE", _customer.CustomerId, null, null);
+            }
+        }
+    }
+}
---
+++ jQuery_WebAPI_StoredProc_CRUD_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jQuery_WebAPI_StoredProc_CRUD_MVC.MvcApplication" Language="C#" %>
